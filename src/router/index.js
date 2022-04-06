6b04c0dfb0c085cc385e8c1d710d6ae62fcd625c
NhatBi <nhatbien99@gmail.com>
2022-04-06 21:54:09 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "../views/ContactBook.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "ContactBook",
+        component: ContactBook,
+    },
+    {
+        path: "/contacts/:id",
+        name: "EditContact",
+        component: () => import("../views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: () => import("../views/NotFound.vue"),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+    
+});
+
+export default router;
